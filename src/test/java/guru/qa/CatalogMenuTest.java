d793958a87b533e5f1a79f3b28a3b7f6cc2fa4cf
EphimSh <ephimizm@gmail.com>
2023-06-24 18:20:24 +0300

    CatalogMenuTest updated

--- src/test/java/guru/qa/CatalogMenuTest.java
+++ src/test/java/guru/qa/CatalogMenuTest.java
@@ -4,7 +4,7 @@ import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -18,7 +18,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
-public class CatalogMenuButtonsTest {
+public class CatalogMenuTest {
 
     static {
         Configuration.pageLoadStrategy = "eager";
